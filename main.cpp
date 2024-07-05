d85409d5a2440143a9dcaac83c95f075ebdd6db4
Uzair05 <ubcasim@gmail.com>
2024-07-05 09:42:21 +0800

    uploaded from local

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
+#include <algorithm>
 #include <iostream>
+#include <numeric>
+#include <set>
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char const *argv[]) {
-    // Add code
+size_t sum_of_multiples(const size_t n, const std::set<size_t>& base) {
+    std::set<size_t> res{};
+    for (size_t i{1}; i < n; i++) {
+        if (std::any_of(base.cbegin(), base.cend(), [i](const size_t& b) { return i % b == 0; })) {
+            res.insert(i);
+        }
+    }
+
+    return std::accumulate(res.cbegin(), res.cend(), 0ul);
+}
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char const* argv[]) {
+    std::cout << sum_of_multiples(1000, std::set<size_t>{3, 5}) << std::endl;
 
     return 0;
 }
