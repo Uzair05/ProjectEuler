5055e131f0efa493391825435ea4bc2eb7da8c23
Uzair05 <ubcasim@gmail.com>
2024-07-05 15:35:49 +0800

    solved

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
+#include <algorithm>
 #include <iostream>
+#include <set>
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char const *argv[]) {
-    // Add code
+
+
+void get_primes(std::set<unsigned long long>& primes, size_t n) {
+    if (primes.empty()) {
+        primes.insert(2);
+    }
+
+    unsigned long long i{*primes.rbegin()};
+    while (primes.size() < n){
+        if (!std::any_of(primes.cbegin(), primes.cend(),
+                         [&i](const auto& p) { return i % p == 0; })) {
+            primes.insert(i);
+        }
+        i++;
+    }
+}
+
+int main() {
+    std::set<unsigned long long> primes{2};
+    
+    get_primes(primes, 10001);
+    std::cout << *primes.rbegin() << "\r\n";
 
     return 0;
 }
