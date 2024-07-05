00be27586ea9b674a4feb865e2fa1a145c683ca8
Uzair05 <ubcasim@gmail.com>
2024-07-05 09:43:14 +0800

    uploaded from local

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
+#include <algorithm>
 #include <iostream>
+#include <numeric>
+#include <set>
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char const *argv[]) {
-    // Add code
+unsigned long long sum_of_fibbonaci(const unsigned long long n, auto fn) {
+    unsigned long long a{}, b{1}, c{2};
+    unsigned long long res{0};
+
+    if (fn(b)) res += b;
+    if (fn(c)) res += c;
+
+    while ((b + c) < n) {
+        a = b;
+        b = c;
+        c = (a + b);
+
+        if (fn(c)) {
+            res += c;
+        }
+    }
+    return res;
+}
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char const* argv[]) {
+    std::cout << sum_of_fibbonaci(4'000'000ull, [](const unsigned long long& a) {
+        return a % 2ull == 0ull;
+    }) << std::endl;
 
     return 0;
 }
