08261d36af4f89cac71969e8e1dbfa2924a2a521
Uzair05 <ubcasim@gmail.com>
2024-07-09 17:33:41 +0800

    fastest possible solution

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@
 
 void get_primes(std::vector<unsigned long long>& primes, unsigned long long n) {
     if (*primes.rbegin() < n) {  // we wish to prevent recalculations for primes
-        std::cout << "Priming for:\t" << n << "\r\n";
         for (auto i{*primes.rbegin() + 1ull}; i <= n; i++) {
             if (!std::any_of(primes.begin(), primes.end(),
                              [&i](const auto& p) { return i % p == 0; })) {
@@ -26,21 +25,23 @@ void get_primes(std::vector<unsigned long long>& primes, unsigned long long n) {
             }
             ++i;
         }
-        std::cout << "Primed for:\t" << *primes.rbegin() << "\r\n";
     }
 }
 
 int main() {
+
+    const unsigned number_of_factors_threshold{500};
+
     unsigned long long num{0};
     unsigned long long size_n{1};
+    unsigned res;
 
-    unsigned res{1};
     std::map<unsigned long long, unsigned> prime_factors{};
     std::vector<unsigned long long> primes{2};
 
-    get_primes(primes, 12372);  // speed up the process; 
+    get_primes(primes, 12372);  // speed up the process; max usuable prime number at 500 thresh is 123723
 
-    while (res < 500) {
+    while (res < number_of_factors_threshold) {
         num += size_n++;
         prime_factors.clear();
         res = 1u;
@@ -66,19 +67,14 @@ int main() {
             }
         }
 
-        // for(const auto& [_,v]: prime_factors){
-        //     res *= (v+1);
-        // }
 
         res = std::accumulate(
             prime_factors.begin(), prime_factors.end(), 1u,
             [](const unsigned& acc, const auto pf) { return acc * (pf.second + 1); });
 
-        std::cout << num << ": " << res << "\r\n";
     }
-
-
     
+
     std::cout << "\r\n\nFinal:\t\t" << num << ": " << res << std::endl;
     return 0;
 }
