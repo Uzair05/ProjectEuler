2c6020f30803f10e86a7373c2d809c129e7dca62
Uzair05 <ubcasim@gmail.com>
2024-07-09 14:45:58 +0800

    get combination

--- main.cpp
+++ main.cpp
@@ -39,33 +39,33 @@ int main() {
     std::map<unsigned long long, int> prime_factors{};
     std::vector<unsigned long long> primes{2};
 
-    while (num<80) {
+    // while (num<80) {
 
-        num += size_n++;
-        get_primes(primes, num);
-        prime_factors.clear();
+    //     num += size_n++;
+    //     get_primes(primes, num);
+    //     prime_factors.clear();
 
-        auto n{num};
-        while(n>1){
-            for(const auto& p: primes){
-                while (n%p == 0){
-                    if (prime_factors.contains(p)){
-                        ++prime_factors.at(p);
-                    }else{
-                        prime_factors.insert(std::pair<unsigned long long, int>(p, 1));
-                    }
-                    n /= p;
-                }
+    //     auto n{num};
+    //     while(n>1){
+    //         for(const auto& p: primes){
+    //             while (n%p == 0){
+    //                 if (prime_factors.contains(p)){
+    //                     ++prime_factors.at(p);
+    //                 }else{
+    //                     prime_factors.insert(std::pair<unsigned long long, int>(p, 1));
+    //                 }
+    //                 n /= p;
+    //             }
 
-                if (n == 1) break;
-            }
-        }
+    //             if (n == 1) break;
+    //         }
+    //     }
 
-        std::cout << num << std::endl;
-        for(const auto& [k,v]: prime_factors){
-            std::cout << k << ", " << v << "\r\n";
-        }
-    }
+    //     std::cout << num << std::endl;
+    //     for(const auto& [k,v]: prime_factors){
+    //         std::cout << k << ", " << v << "\r\n";
+    //     }
+    // }
 
     for (auto i{0ull}; i < 21; i++) {
         std::cout << combination(20, i) << std::endl;
