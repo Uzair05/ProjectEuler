03578801a7ee2a2c1f03b56980b8db6684c12b44
Uzair05 <ubcasim@gmail.com>
2024-07-10 10:03:11 +0800

    better held

--- main.cpp
+++ main.cpp
@@ -1,35 +1,37 @@
 #include <iostream>
-#include <vector>
-#include <numeric>
+
+struct counts_ {
+    unsigned count{0};
+    unsigned start_number{0};
+};
 
 int main() {
+    auto even_fn = [](const unsigned n) { return n / 2u; };
+    auto odd_fn = [](const unsigned n) { return ((n * 3u) + 1u); };
 
-    auto even_fn = [](const unsigned n){return n/2u;};
-    auto odd_fn =  [](const unsigned n){return ((n*3u)+1u);};
-    
-    std::vector<unsigned> counts_{};
+    counts_ count_{};
     unsigned count{};
-    
-    for(unsigned i{0}; i<1'000'000; i++){
+
+    for (unsigned i{0}; i < 1'000'000; i++) {
         count = 0;
 
         unsigned i_{i};
-        while(i_ > 1){
-            if ((i_%2)==0){
+        while (i_ > 1) {
+            if ((i_ % 2) == 0) {
                 i_ = even_fn(i_);
-            }else{
+            } else {
                 i_ = odd_fn(i_);
             }
             ++count;
         }
 
-        counts_.push_back(count);
+        if (count_.count < count) {
+            count_.count = count;
+            count_.start_number = i;
+        }
     }
 
-
-    std::cout << 
-        std::distance(counts_.begin(), std::find(counts_.begin(), counts_.end(), std::accumulate(counts_.begin(), counts_.end(), 0u, [](auto acc, auto& n){return ((acc>n)?acc:n);})))
-        << "\r\n";
+    std::cout << count_.start_number << "\r\n";
 
     return 0;
 }
