16bf29376caa601f56ca1885a111e50d67bb05a1
Uzair05 <ubcasim@gmail.com>
2024-07-31 11:03:20 +0800

    Can make and display massive factorials

--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <deque>
+
+void multiply(std::deque<unsigned>& res, unsigned n) {
+    auto carry{0u};
+    for(auto& i: res){
+        i = (i*n + carry);
+        carry = i/10u;
+        i = i%10u;
+    }
+
+    while (carry > 0){
+        res.push_back(carry%10u);
+        carry = carry/10u;
+    }
+}
+
+
+void factorial(std::deque<unsigned>& res, unsigned n){
+    while (n>1){
+        multiply(res, n);
+        n-=1u;
+    }
+}
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char const *argv[]) {
-    // Add code
+    
+    std::deque<unsigned> num{1};
+    factorial(num, 99);
+
+    for (auto it{num.rbegin()}; it!=num.rend(); it++){
+        std::cout << *it;
+    }std::cout << "\n";
 
     return 0;
 }
