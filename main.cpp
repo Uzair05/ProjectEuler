e9c2eab4cb08b074f219e89e6a28de046d7ea73d
Uzair05 <ubcasim@gmail.com>
2024-08-01 15:07:20 +0800

    Concise

--- main.cpp
+++ main.cpp
@@ -24,23 +24,18 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char const* argv[]) {
     file.close();
     std::sort(names_list.begin(), names_list.end());
 
+    auto idx{0ul};
     std::vector<unsigned long> score{};
     std::transform(names_list.begin(), names_list.end(), std::back_inserter(score),
-                   [](const std::string& name) {
+                   [&idx](const std::string& name) {
                        unsigned long res{0};
                        for (const char& c : name) {
                            if ((c >= 'A') && (c <= 'Z')) {
                                res += (static_cast<unsigned long>(c - 'A') + 1);
                            }
                        }
-                       return res;
+                       return (res * ++idx);
                    });
-
-    for (size_t i{0}; i < score.size(); i++) {
-        score[i] *= (i + 1ul);
-    }
-
-    std::cout << names_list.size() << " " << score.size() << "\n";
 
     std::cout << std::accumulate(score.begin(), score.end(), 0ul) << "\n";
     std::cout << "correct answer:\t" << 871198282 << "\n";
