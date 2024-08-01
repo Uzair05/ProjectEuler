a8ca4b748722abcef7e9dd6301fb8b339ca9f669
Uzair05 <ubcasim@gmail.com>
2024-08-01 12:29:02 +0800

    Formatted

--- main.cpp
+++ main.cpp
@@ -1,46 +1,44 @@
+#include <algorithm>
+#include <fstream>
 #include <iostream>
+#include <numeric>
 #include <string>
 #include <vector>
-#include <fstream>
-#include <algorithm>
-#include <numeric>
 
-
-int main([[maybe_unused]] int argc, [[maybe_unused]] char const *argv[]) {
+int main([[maybe_unused]] int argc, [[maybe_unused]] char const* argv[]) {
     std::fstream file;
     file.open("names.txt", std::ios::in);
 
-
-    if (!file.is_open()){
-        std::cout << "File Open Error" << "\n";
+    if (!file.is_open()) {
+        std::cout << "File Open Error"
+                  << "\n";
         return 1;
     }
 
     std::vector<std::string> names_list{};
     std::string buffer{};
-    while (std::getline(file, buffer, ',')){
+    while (std::getline(file, buffer, ',')) {
         names_list.push_back(buffer);
     }
     std::sort(names_list.begin(), names_list.end());
 
     std::vector<unsigned long> score{};
-    std::transform(names_list.begin(), names_list.end(), std::back_inserter(score), [](const std::string& name){
-        unsigned long res{0};
-        for(const char& c: name){
-            if ((c >= 'A') && (c <= 'Z')){
-                res += (static_cast<unsigned long>(c - 'A') + 1);
-            }
-        }
-        return res;
-    });
-    
+    std::transform(names_list.begin(), names_list.end(), std::back_inserter(score),
+                   [](const std::string& name) {
+                       unsigned long res{0};
+                       for (const char& c : name) {
+                           if ((c >= 'A') && (c <= 'Z')) {
+                               res += (static_cast<unsigned long>(c - 'A') + 1);
+                           }
+                       }
+                       return res;
+                   });
 
-    for(size_t i{0}; i<score.size(); i++){
-        score[i] *= (i+1ul);
+    for (size_t i{0}; i < score.size(); i++) {
+        score[i] *= (i + 1ul);
     }
 
     std::cout << std::accumulate(score.begin(), score.end(), 0ul) << "\n";
-
 
     return 0;
 }
