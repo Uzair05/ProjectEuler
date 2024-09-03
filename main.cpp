817a00b597e61d5afd932f36bcfcffa21e593691
Uzair05 <ubcasim@gmail.com>
2024-09-03 12:06:15 +0800

    Cleanup

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <algorithm>
 #include <iostream>
-#include <vector>
 #include <numeric>
+#include <vector>
 
 template <typename T>
 void generate_primes(std::vector<T>& primes, unsigned range) {
@@ -17,41 +17,35 @@ void generate_primes(std::vector<T>& primes, unsigned range) {
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char const* argv[]) {
     std::vector<unsigned long long> primes{};
-    std::vector<unsigned long long> truncate{};
-
-
     generate_primes(primes, 800'000u);
 
-    // test for truncate right --> easier
     auto sum{0ull};
     for (auto p_ : primes) {
-        
         if (p_ > 10) {
             auto p(p_);
             bool flag = true;
             while ((p > 0) && flag) {
                 p = p / 10ull;
-                if ((p>0) && (!std::binary_search(primes.begin(), primes.end(), p))) {
+                if ((p > 0) && (!std::binary_search(primes.begin(), primes.end(), p))) {
                     flag = false;
                 }
             }
 
             auto rp_{p_};
-            auto max_trunc_range(100'000ull); // number of zeros possible
-            while ((rp_ > 0) && (flag)){
-                auto rp__ = rp_%max_trunc_range;
-                if (rp__ == rp_){
+            auto max_trunc_range(100'000ull);  // number of zeros possible
+            while ((rp_ > 0) && (flag)) {
+                auto rp__ = rp_ % max_trunc_range;
+                if (rp__ == rp_) {
                     max_trunc_range /= 10ull;
-                }else{
+                } else {
                     rp_ = rp__;
 
-                    if ((rp_>0) && (!std::binary_search(primes.begin(), primes.end(), rp_))){
+                    if ((rp_ > 0) && (!std::binary_search(primes.begin(), primes.end(), rp_))) {
                         flag = false;
                     }
                 }
             }
 
-            
             if (flag) sum += p_;
         }
     }
