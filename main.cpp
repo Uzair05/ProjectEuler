757a0ecc92f9ed9da75160025091df4f55e4a2cd
Uzair05 <ubcasim@gmail.com>
2024-09-11 15:52:03 +0800

    Solved

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 
+unsigned square_chain(unsigned n) {
+    unsigned res{0u};
+    while (n > 0) {
+        res += ((n % 10u) * (n % 10u));
+        n /= 10u;
+    }
+    return res;
+}
+
 int main([[maybe_unused]] int argc, [[maybe_unused]] char const *argv[]) {
-    // Add code
+    size_t counter{0u};
+    for (unsigned i{2u}; i < 10'000'000u; i++) {
+        unsigned n{i};
+        while ((n != 1) && (n != 89)) {
+            n = square_chain(n);
+        }
+        if (n == 89) ++counter;
+    }
+
+    std::cout << counter << std::endl;
 
     return 0;
 }
