08626fb6a1698fb86b19bd9b282558f760057695
Uzair05 <ubcasim@gmail.com>
2024-09-24 17:40:04 +0800

    Getting Somewhere

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@ using ull = unsigned long long;
 
 ull inc_denom(unsigned digit, unsigned depth, std::map<unsigned, std::map<unsigned, ull>>& m) {
     if (depth == 1u) return 1ull;
-    if (digit == 0u) return 1ull;
 
     if (m.contains(depth) && m.at(depth).contains(digit)) {
         return m.at(depth).at(digit);
@@ -27,18 +26,11 @@ ull inc_denom(unsigned digit, unsigned depth, std::map<unsigned, std::map<unsign
 
 ull dec_denom(unsigned digit, unsigned depth, std::map<unsigned, std::map<unsigned, ull>>& m) {
 
-    std::cout << "Enter:\t" << digit << ",\t" << depth << "\n";
-
     if (depth == 1u) return 1ull;
-    if (digit == 9u) return 1ull;
-
-    std::cout << "Passed Terminals\n";
-
+    
     if (m.contains(depth) && m.at(depth).contains(digit)) {
         return m.at(depth).at(digit);
     }
-
-    std::cout << "Passed Memoization\n";
 
     ull res{0ull};
     for (unsigned idx{digit}; idx <= 9u; idx++) res += dec_denom(idx, depth - 1, m);
@@ -57,19 +49,22 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char const* argv[]) {
     std::map<unsigned, std::map<unsigned, ull>> m_1{};
     ull acc_i{inc_denom(1, 11, m_1)};
     for (auto i{10u}; i > 0u; i--) {
-        acc_i += inc_denom(9, i, m_1);
+        acc_i += inc_denom(9u, i, m_1);
     }
     m_1.clear();
 
     std::map<unsigned, std::map<unsigned, ull>> m_2{};
-    ull acc_d{dec_denom(1, 11, m_2)};
-    for (auto i{10u}; i > 0u; i--) {
-        acc_d += dec_denom(0, i, m_2);
+    // ull acc_d{dec_denom(1, 11, m_2)};
+    ull acc_d{0ull};
+    for (auto i{11u}; i > 0u; i--) {
+        acc_d += dec_denom(0u, i, m_2);
     }
+
     m_2.clear();
 
     std::cout << acc_i + acc_d << "\n";
     std::cout << std::numeric_limits<ull>::max() << "\n";
-
+    std::cout << dec_denom(9u, 11u, m_2); << "\n";
+    
     return 0;
 }
