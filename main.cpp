813e9f933cc47b11d146841116ba1d7731b7dcce
Uzair05 <ubcasim@gmail.com>
2024-10-08 10:29:13 +0800

    still stuck

--- main.cpp
+++ main.cpp
@@ -4,64 +4,38 @@
 
 using ull = unsigned long long;
 
-ull inc_denom(unsigned digit, unsigned depth, std::map<unsigned, std::map<unsigned, ull>>& m) {
-    if (depth == 1u) return 1ull;
-
-    if (m.contains(depth) && m.at(depth).contains(digit)) {
-        return m.at(depth).at(digit);
-    }
-
-    ull res{0ull};
-    for (unsigned idx{0u}; idx <= digit; idx++) res += inc_denom(idx, depth - 1, m);
-
-    if (m.contains(depth)) {
-        m.at(depth).insert(std::pair<unsigned, ull>(digit, res));
-    } else {
-        auto m_ = std::map<unsigned, ull>{{digit, res}};
-        m.insert(std::pair<unsigned, std::map<unsigned, ull>>(depth, m_));
-    }
-
-    return res;
-}
-
-ull dec_denom(unsigned digit, unsigned depth, std::map<unsigned, std::map<unsigned, ull>>& m) {
-
-    if (depth == 1u) return 1ull;
+ull inc_denom(std::map<unsigned, std::map<unsigned, ull>>& m, unsigned depth, unsigned digit=1u, [[maybe_unused]] bool init=true) {
     
-    if (m.contains(depth) && m.at(depth).contains(digit)) {
-        return m.at(depth).at(digit);
+    if (depth == 1) return 1;
+    if (digit == 9u) return 1;
+    if (m.contains(depth) && m.at(depth).contains(digit)) return m.at(depth).at(digit);
+
+    auto res{0ull};
+
+    if (init){
+        for(auto j{1u}; j<=9u; j++){
+            for(auto i{j}; i <= 9u; i++){
+                res += inc_denom(m, depth-1, i, false);
+            }
+        }
     }
 
-    ull res{0ull};
-    for (unsigned idx{digit}; idx <= 9u; idx++) res += dec_denom(idx, depth - 1, m);
 
-    if (m.contains(depth)) {
-        m.at(depth).insert(std::pair<unsigned, ull>(digit, res));
-    } else {
-        auto m_ = std::map<unsigned, ull>{{digit, res}};
-        m.insert(std::pair<unsigned, std::map<unsigned, ull>>(depth, m_));
+    for(auto i{digit}; i <= 9u; i++){
+        res += inc_denom(m, depth-1, i, false);
     }
-
+    m.insert(std::pair(depth, std::map<unsigned, ull>{{digit, res}}));
     return res;
+    
 }
+
+
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char const* argv[]) {
-    std::map<unsigned, std::map<unsigned, ull>> m_1{};
+    std::map<unsigned, std::map<unsigned, ull>> m{};
 
-    ull acc_i{0ull};
-    for (auto i{6u}; i > 3u; i--) {
-        acc_i += inc_denom(9u, i, m_1);
-    }
-    m_1.clear();
-
-    ull acc_d{0ull};
-    for (auto i{7u}; i > 4u; i--) {
-        acc_d += dec_denom(0u, i, m_1);
-    }
-    m_1.clear();
-
-    std::cout << acc_i + acc_d + dec_denom(1u, 7u, m_1) << "\n"; // Slightly less than 277032 (-30); Consider non bouncy under 1000
-    std::cout << std::numeric_limits<ull>::max() << "\n";
+    std::cout << inc_denom(m, 2) << "\n";
+   
     
     return 0;
 }
